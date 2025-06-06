35c4389f30adb670ee93df08c961b2b1190fd650
Theresa Ng <theashaomin@outlook.com>
2025-06-06 09:55:12 +0800

    Removed duplicated app

--- expensetracker/src/api/LocationList.jsx
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-const BASE_URL = 'https://api.countrystatecity.in/v1/countries/%5Bciso%5D/cities';
-const locationList = axios.create({baseURL:BASE_URL});
-
-export default locationList;
