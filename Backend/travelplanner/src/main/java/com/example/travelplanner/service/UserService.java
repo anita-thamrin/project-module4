6988980c63f71ba0de94f91e8c21df4bec412223
Anita <anita.thamrin@gmail.com>
2025-08-01 22:42:32 +0800

    added user

--- Backend/travelplanner/src/main/java/com/example/travelplanner/service/UserService.java
+++ Backend/travelplanner/src/main/java/com/example/travelplanner/service/UserService.java
@@ -1,5 +1,17 @@
 package com.example.travelplanner.service;
 
-public class UserService {
+import java.util.List;
+
+import com.example.travelplanner.entity.User;
+
+public interface UserService {
+    User createUser(User user);
+    User getUser(Long id);
+    List<User> getAllUsers();
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
+
+    List<User> findByFirstNameContainingIgnoreCase(String firstName);
+    List<User> findByFirstNameIgnoreCase(String firstName);
     
 }
